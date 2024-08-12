7f9fb797b63cc66ad07c1cfd3746fa6c8a93c9b9
Your Name <gemmaw09lo@gmail.com>
2024-08-12 23:08:29 +0100

    added user plays to best of 3

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -11,14 +11,17 @@ possible_hands = ["rock", "paper", "scissors"]
 def computers_hand():
     return random.choice(possible_hands)
 
-
 def users_hand():
     return input("Enter rock, paper or scissors: ")
 
-def play_again():
+def ask_for_another_game():
     return input("Play again?(y/n): ")
 
+
 def game():
+    user_score = 0
+    computer_score = 0
+
     while True:
         user = users_hand()
         computer = computers_hand()
@@ -28,16 +31,34 @@ def game():
             print("Draw")
         elif user == "rock" and computer == "scissors":
             print("You win!")
+            user_score += 1
         elif user == "paper" and computer == "rock":
             print("You win!")
+            user_score += 1
         elif user == "scissors" and computer == "paper":
             print("You win!")
+            user_score += 1
         else:
             print("You lose!")
+            computer_score += 1
+        print ("Computer {}:{} You".format(computer_score, user_score))
 
-        play_again = input("Play again?(y/n): ")
-        if play_again == "n":
-            break
+        if computer_score == 2:
+            print("Computer wins - better luck next time!")
+            play_again = ask_for_another_game()
+            if play_again == "n":
+                break
+            else:
+                computer_score = 0
+                user_score = 0
+        elif user_score == 2:
+            print("You win! Congratulations!")
+            play_again = ask_for_another_game()
+            if play_again == "n":
+                break
+            else:
+                computer_score = 0
+                user_score = 0
 
 
 game()
